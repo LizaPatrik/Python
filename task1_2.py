24139de17937effd75b6f7288a4bf008db8b36f6
LizaPatrik <55915741+LizaPatrik@users.noreply.github.com>
2020-07-16 02:14:19 +0500

    Create task1_2.py

---
+++ task1_2.py
@@ -0,0 +1,35 @@
+my_list = input("Введите ФИО, разделяя пробелами, без запятых.")
+age = int(input("Введите возраст, разделяя пробелами, без запятых."))
+height = int(input("Введите рост(см), разделяя пробелами, без запятых."))
+weight = float(input("Введите вес(кг), разделяя пробелами, без запятых."))
+i = 0
+
+my_list = my_list.split()#объединение всех введенных параметров в список
+my_list.append(age)
+my_list.append(height)
+my_list.append(weight)
+
+
+if len(my_list) != 6:
+    print("Возможно Вы что-то забыли указать, попробуйте ввести ещё раз.")#проверка введения всех данных
+else:
+    print("В списке:", my_list)#вывод типов
+    while i < len(my_list):
+       print("Тип", i+1,"-го элемента", type(my_list[i]))
+       i += 1
+
+#__________________________________________________
+
+#my_list = ['dh', 'hdfhdg', 'dhdghf', 1, 1, 1.0, 'fgdfg'] - для проверки списков с нечетной длинной
+i = 0
+if len(my_list) == 0:
+    my_list = input("Вы ввели пустой список, попробуйте ещё раз.")
+elif len(my_list) == 1:
+    print("В вашем списке всего один пункт:", my_list)
+else:
+    while i < (len(my_list) - 1):
+        t = my_list[i]
+        my_list[i] = my_list[i + 1]
+        my_list[i + 1] = t
+        i += 2
+print(my_list)
